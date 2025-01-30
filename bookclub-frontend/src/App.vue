8d5a4b1ad907a825e4be5615f3d22160f41a2421
Lyn Sabiston <83124470+LynSab@users.noreply.github.com>
2025-01-30 01:13:07 +0000

    set-up router and add login template

--- bookclub-frontend/src/App.vue
+++ bookclub-frontend/src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-
+  import { RouterView } from 'vue-router'
 </script>
 
 <template>
- <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <RouterView></RouterView>
 </template>
 
 <style scoped>
