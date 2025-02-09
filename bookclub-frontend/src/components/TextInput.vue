42bbd20e5e12b5972f179b1c6780d5bec6aac918
Lyn Sabiston <83124470+LynSab@users.noreply.github.com>
2025-02-09 21:08:11 +0000

    adjust positioning and add button

--- bookclub-frontend/src/components/TextInput.vue
+++ bookclub-frontend/src/components/TextInput.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="box-content size-10">
+  <div>
     <fieldset class="block m-5 text-base border rounded-md border-slate-600">
       <legend class="mt-3 ml-3">{{props.legendText}}</legend>
       <input class="outline-none" />
--- bookclub-frontend/src/views/Login.vue
+++ bookclub-frontend/src/views/Login.vue
@@ -3,11 +3,17 @@
 </script>
 
 <template>
-  <div class="box-content shadow-lg size-80 border-2 rounded-md m-auto p-5">
-    <h1 class="m-5 text-2xl font-semibold">Login</h1>
-     
-    <TextInput legend-text="Email"/>
-    <TextInput legend-text="Password"/>
+  <div class="grid grid-rows-3 h-screen place-content-center">
+    <div class="text-5xl text-slate-600 col-start-2 self-center">
+      <h1>Bookclub App</h1>
+    </div>
+
+    <div class="shadow-lg border-2 rounded-md text-slate-600 bg-white row-start-2 col-start-2">
+      <h1 class="m-4 text-2xl font-semibold">Login</h1>
+      <TextInput legend-text="Email"/>
+      <TextInput legend-text="Password"/> 
+      <button class="shadow-lg border-1 rounded-md p-1 ml-5 cursor-pointer active:bg-slate-200">Log in</button>
+    </div>
   </div>
 </template>
 
