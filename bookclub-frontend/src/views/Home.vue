ee336992cb2cc80671845a7c8d07064701e2b47c
Lyn Sabiston <83124470+LynSab@users.noreply.github.com>
2025-03-25 00:00:11 +0000

    create base template of home page

--- bookclub-frontend/src/views/Home.vue
+++ bookclub-frontend/src/views/Home.vue
@@ -2,7 +2,25 @@
 </script>
 
 <template>
-<h2>HOME PAGE</h2>
+  <div class = "grid-cols-1 border p-5">
+    <div class="col-1">
+      <h1 class="text-6xl font-bold text-center text-cyan-800">chapters.</h1>
+      <button class="fixed top-3 right-3 shadow-lg border-1 rounded-md p-1 ml-5 cursor-pointer active:bg-slate-200">Account</button>
+    </div>
+  </div>
+
+  <div class="grid grid-cols-5 rows-1">
+    <div class="min-h-[calc(100vh-102px)] border">
+      <button class="w-full shadow-lg border-1 p-2 cursor-pointer active:bg-slate-200">
+        My Bookclubs
+      </button>
+
+      <button class="w-full shadow-lg border-1 p-2 cursor-pointer active:bg-slate-200">
+        Create a new club
+      </button>
+    </div>
+    <div class="col-span-4 p-3">main content</div>
+  </div>
 </template>
 
 <style scoped>
