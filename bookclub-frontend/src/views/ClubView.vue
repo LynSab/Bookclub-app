003b3920af76b0a0d703f5a2611a6d527d906e7b
Lyn Sabiston <83124470+LynSab@users.noreply.github.com>
2025-04-20 01:53:41 +0100

    add views for Clubs, ClubView and Signup

---
+++ bookclub-frontend/src/views/ClubView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useRoute } from 'vue-router';
+import { getClubRecord } from '../dummy-book-clubs';
+const route = useRoute()
+const currentClub = getClubRecord(route.params.clubId)
+</script>
+
+<template>
+  <h3 class="text-xl font-semibold">{{ currentClub.name }}</h3>
+  <div>
+    <h4 class="text-m font-semibold">Members:</h4>
+    <ul>
+      <li v-for="member in currentClub.members">{{ member }}</li>
+    </ul>
+    <h4 class="text-m font-semibold">Current Book: </h4>
+    {{ currentClub.book.name }} by {{ currentClub.book.author }} 
+      <h4 class="text-m font-semibold">Next Meeting Date: </h4>
+      {{ currentClub.meeting }}
+  </div>
+</template>
+
+<style scoped></style>
---
+++ bookclub-frontend/src/views/Clubs.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import { ref } from 'vue'
+  import router from '../router';
+  import { bookclubs } from '../dummy-book-clubs';
+</script>
+
+<template>
+  <h3 class="text-xl font-semibold">My Clubs</h3>
+  <div class="p-3" v-for="bookclub in bookclubs">
+    <button @click="router.push({ name: 'ClubView', params: {clubId: bookclub.id}})" class="cursor-pointer active:bg-slate-200">{{ bookclub.name }}</button>
+  </div>
+</template>
+
+<style scoped></style>
