b78753a4df956cc3b89359dac01bdedb0e5a82ae
Lyn Sabiston <83124470+LynSab@users.noreply.github.com>
2025-05-12 23:19:39 +0100

    add main bookclub endpoint

---
+++ bookclub-backend/dummy-book-clubs.js
@@ -0,0 +1,20 @@
+const bookclubs = [
+  {name:'Club 1', id: 1, members: ['Billy', 'Bob', 'Barbara', 'Benny', 'Becky'], book: {name: 'Magician', author: 'Raymond E Feist'}, meeting: '24/05/25'}, 
+  {name:'Club 2', id: 2, members: ['Charlie', 'Cameron', 'Carly', 'Casandra', 'Constance'], book: {name: 'Redshirts', author: 'John Scalzi'}, meeting: '30/05/25'}, 
+  {name:'Club 3', id: 3, members: ['Diane', 'Davis', 'Danny', 'Dierdre', 'Derek'], book: {name: 'Sword of Shanarah', author: 'Terry Brooks'}, meeting: '27/05/25'}, 
+  {name:'Club 4', id: 4, members: ['Erik', 'Erin', 'Eustance', 'Ewan', 'Erwing'], book: {name: 'John Dies at the End', author: 'David Wong'}, meeting: '22/05/25'}
+]
+
+function getClubRecord(id) {
+  let recordByID = {}
+
+  for (let club of bookclubs) {
+    if (club.id == id) {
+      recordByID = club
+    }
+  }
+
+  return recordByID
+}
+
+module.exports =  { getClubRecord, bookclubs };
